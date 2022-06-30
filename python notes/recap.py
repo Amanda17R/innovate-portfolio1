a05b4238a7caa63cba33e2efa6c106d409339cd0
Amanda Richards <grumpybag@gmail.com>
2022-06-30 11:55:56 +0100

    front notes html

---
+++ python notes/recap.py
@@ -0,0 +1,33 @@
+from logging import NOTSET
+from os import mkdir
+import pwd
+
+
+print("this is a string")
+print("this is my file")
+
+greeting = "hello everyone"
+
+print (greeting)
+
+
+print ("this is a string for displaying charachters")
+print("1234") #this is a string
+print(1234+1) #this is a integer/ whole number
+print(12.34) #this is a float
+print(True) #one of the boolean data values
+print(False) # one of the booleans
+print(None) #none-blank/null data 
+#import random
+print("this is a string for displaying charachters")
+print("1234") #this is a string
+print(1234+1) #this is a integer/whole number
+print(12.34) #this is a float
+print(True) #one of the boolean data values
+print(False) #one of the booleans it is or isnt 
+print(None) #none- blank /null data 
+print(len(greeting))
+
+
+
+
